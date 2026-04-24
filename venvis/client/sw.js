99e2de6af32038abfc19f36a184c4fb6d4d2a6cb
Charly <orvex.aiii@gmail.com>
2026-04-23 21:50:13 -0300

    fix(venvis): bump SW cache to v3 to force client update

--- venvis/client/sw.js
+++ venvis/client/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'venvis-v2'
+const CACHE = 'venvis-v3'
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon.svg']
 
 self.addEventListener('install', (e) => {
